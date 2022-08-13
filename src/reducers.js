43a7c914de77091f4fc69749230d6ff335d32819
Deen John <dj.itbhu@gmail.com>
2022-08-13 21:51:23 +0530

    Create reducers.js

---
+++ src/reducers.js
@@ -0,0 +1,10 @@
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+      case 'INC':
+        return state + 1;
+      case 'DEC':
+        return state - 1;
+      default:
+        return state;
+    }
+}; 
