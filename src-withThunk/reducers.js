5b11339e838ac2613289d266b2fb53a35250e970
Deen John <dj.itbhu@gmail.com>
2022-08-14 00:37:34 +0530

    Create reducers.js

---
+++ src-withThunk/reducers.js
@@ -0,0 +1,29 @@
+import { ACTIONS_TYPES} from "./actions/types";
+const initialStateCounter = {
+  isLoading: false,
+  count: 0
+};
+
+export const counterReducer = (state=initialStateCounter , action) => {
+    switch (action.type) {
+      case ACTIONS_TYPES.INC:
+        return {
+          isLoading: false,
+          count: state.count + 1
+        };
+      case ACTIONS_TYPES.DEC:
+        return {
+          isLoading: false,
+          count: state.count - 1
+        };
+      case ACTIONS_TYPES.LOADING:
+        return {
+          ...state,
+          isLoading: true,
+        };
+      default:
+        return state;
+    }
+}; 
+
+
